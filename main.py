d2adead1828f0a36b31c758938450323c09057e3
Олександр <103941056+daosRage@users.noreply.github.com>
2026-08-14 23:59:26 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+from math import hypot
+from random import randint
+import pygame
+
+#налаштування гри
+WIDTH = 1000          #ширина вікна
+HEIGHT = 1000         #висота вікна
+FOOD_COUNT = 1000     #скільки кружечків-їжі буде на полі
+WORLD_SIZE = 4000     #де можуть з'явитися кружечки (від -4000 до 4000)
+PLAYER_SPEED = 15     #швидкість руху гравця
+FPS = 60              #скільки кадрів за секунду
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("З'їж кружечки!")
+clock = pygame.time.Clock()
+
+#Гравець
+player_x = 0
+player_y = 0
+player_radius = 20
+player_color = (0, 255, 0)  # зелений
+
+
+
+
+
+
+
+
+
+#головний цикл гри
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    move_player(keys)
+    eat_food()
+    draw_everything()
+
+    clock.tick(FPS)
+    pygame.display.flip()
+
+pygame.quit()
