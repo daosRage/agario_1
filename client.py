72cc8ee0f9708307801138df185d4ed62abd0cad
david355 <davidssulga55@gmail.com>
2026-09-12 10:44:07 +0300

    Update client.py

--- client.py
+++ client.py
@@ -17,4 +17,18 @@ def calculate_camera_scale():
     else:
         a = threshold_radius / player_radius
         b = a ** zoom_out_speed
+        return b 
+
+
+def calculate_food_scale():
+    comfortble_scale = 1.0
+    threshold_radius = 100
+    zoom_out_speed = 0.15
+
+    if player_radius <= threshold_radius:
+        return comfortble_scale
+
+    else:
+        a = threshold_radius / player_radius
+        b = a ** zoom_out_speed
         return b  
