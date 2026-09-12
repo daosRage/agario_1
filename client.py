668adfb36862552276fc537e4e33d5b3512b73e8
david355 <davidssulga55@gmail.com>
2026-09-12 10:42:20 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,20 @@
 
+
+
+
+
+
+
+
+def calculate_camera_scale():
+    comfortble_scale = 1.0
+    threshold_radius = 100
+    zoom_out_speed = 0.8
+
+    if player_radius <= threshold_radius:
+        return comfortble_scale
+
+    else:
+        a = threshold_radius / player_radius
+        b = a ** zoom_out_speed
+        return b  
